cf32304a64c6f06409c8cb19faf724404b4a2294
MarcosGOFavaretto <marcos.favaretto@etec.sp.gov.br>
2021-03-05 01:00:51 -0300

    :art:STYLE: Remove as anotações da aula e otimiza comentários!
    Remove as anotações da aula e otimiza comentários da classe principal do
    programa fornecendo informações como o nome do projeto, o autor e a data

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,45 +1,35 @@
-/*
- * Programa Calculadora
- * Autor: Marcos Gabriel De Oliveira Favaretto
- * Aula de D.S. 01/08/2019
+/**
+ * Projeto: Calculadora
+ * Autor: Marcos Gabriel De Oliveira Favaretto.
+ * Data: 01 de Agosto de 2019.
  */
 package calculadora;
 
-/**
- *
- * @author ETEC ALUNO
- */
 public class Calculadora {
-    static int num1 = 10, num2 = 5;
-    
 
-    /**
-     * @param args the command line arguments
-     */
+    static int num1 = 10, num2 = 5;
+
     public static void main(String[] args) {
-        //Exercício 1 - mostrar na tela a soma de dois números:
         somar();
-        
-        //Exercício 2 - mostrarr na tela a subtração de dois números:
         subtrair();
-        
-        //Exercício 3 - mostrarr na tela a multiplicação e a divisão de dois números:
         multiplicar();
         dividir();
     }
-    
-    //Exercício 4 - Criar um método para cada operação:
-    public static void somar(){
+
+    public static void somar() {
         System.out.println(num1 + "+" + num2 + " = " + (num1 + num2));
     }
-    public static void subtrair(){
+
+    public static void subtrair() {
         System.out.println(num1 + "-" + num2 + " = " + (num1 - num2));
     }
-    public static void multiplicar(){
+
+    public static void multiplicar() {
         System.out.println(num1 + "*" + num2 + " = " + (num1 * num2));
     }
-    public static void dividir(){
+
+    public static void dividir() {
         System.out.println(num1 + "/" + num2 + " = " + (num1 / num2));
     }
-    
+
 }
